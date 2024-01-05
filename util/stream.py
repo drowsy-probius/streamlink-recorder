4a986f198e45e7ee827377774bea8b6b6b50d7f2
k123s456h <k123s456h@gmail.com>
2024-01-05 21:34:38 +0900

    force reinstall streamlink

--- util/stream.py
+++ util/stream.py
@@ -5,15 +5,23 @@ from .common import run_command_and_get_stdout
 
 def install_streamlink(streamlink_github=None, streamlink_commit=None, streamlink_version=None):
     if streamlink_github:
-        return run_command_and_get_stdout(f'''{sys.executable} -m pip install "git+{streamlink_github}"''')
+        return run_command_and_get_stdout(
+            f'''{sys.executable} -m pip install --upgrade --force-reinstall "git+{streamlink_github}"'''
+        )
     
     if streamlink_commit:
-        return run_command_and_get_stdout(f'''{sys.executable} -m pip install "git+https://github.com/streamlink/streamlink.git@{streamlink_commit}"''')
+        return run_command_and_get_stdout(
+            f'''{sys.executable} -m pip install --upgrade --force-reinstall "git+https://github.com/streamlink/streamlink.git@{streamlink_commit}"'''
+        )
     
     if streamlink_version:
-        return run_command_and_get_stdout(f'''{sys.executable} -m pip install "streamlink=={streamlink_version}"''') 
+        return run_command_and_get_stdout(
+            f'''{sys.executable} -m pip install --force-reinstall "streamlink=={streamlink_version}"'''
+        ) 
     
-    return run_command_and_get_stdout(f'''{sys.executable} -m pip install --upgrade streamlink''') 
+    return run_command_and_get_stdout(
+        f'''{sys.executable} -m pip install --upgrade --force-reinstall streamlink'''
+    ) 
 
 
 def get_stream_info(target_url: str):
